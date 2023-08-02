28f0d68fa9e3bc23adf2e1bdb7cb1088c8e5dd2c
unknown <jorge.orozco@cetys.edu.mx>
2023-08-01 18:40:44 -0700

    PrismaClient crated for equipmentsCategories' CRUD

--- routes/equipmentCategories.js
+++ routes/equipmentCategories.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { PrismaClient } = require('@prisma/client');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.json({});
+const prisma = new PrismaClient();
+
+// create Category
+router.get('/', async function(req, res, next) {
+  try {
+    const category = await prisma.equipmentCategories.findMany();
+    res.status(200).json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+// create Category
+router.post('/', async function(req, res, next) {
+  try {
+    const category = await prisma.equipmentCategories.create({
+      data: req.body,
+    });
+    res.status(201).json(category);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
